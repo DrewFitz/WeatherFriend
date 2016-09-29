2e375397a2c00354db0dee75d46f4c626f942973
Drew Fitzpatrick <drwftz@gmail.com>
2016-09-29 13:04:53 -0400

    Documented users_controller routes, added username uniqueness validation to user model.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,19 @@
-import HTTParty
+import HTTParty # required to make weather API calls
 
 class UsersController < ApplicationController
 
+	# route: GET / (root)
 	def hello
-		# do nothing
+		# do nothing, just present the login form
 	end
 
+	# route: POST /login
 	def login
+		# see if the user exists already
 		@user = User.find_by name: params[:user][:name]
 
+		# route to confirmation page if user is new
+		# if user exists, present their locations
 		if @user != nil
 			redirect_to "/#{params[:user][:name]}"
 		else
@@ -16,30 +21,46 @@ class UsersController < ApplicationController
 		end
 	end
 
+	# route: GET /new/:name
+	def new
+		# do nothing, just show confirmation page
+	end
+
+	# route: POST /create/:name
 	def create
+		# make the user and show their page
 		@user = User.create name: params[:name]
 		redirect_to "/#{params[:name]}"
 	end
 
+	# route: GET /:name
 	def show
+		# ensure user exists
 		@user = User.find_by name: params[:name]
 
+		# if user doesn't exist, show login screen
 		if @user == nil
 			redirect_to '/'
 		end
 	end
 
+	# route: POST /:name/add_location
 	def add_location
+		# create the location and associate it with the user
 		@user = User.find_by name: params[:name]
 		@location = Location.create city: params[:location][:city], state: params[:location][:state], user: @user
+		# show the user their list of locations
 		redirect_to "/#{params[:name]}"	
 	end
 
+	# NOT IMPLEMENTED IN UI YET
+	# route: DELETE /:name/:location
 	def remove_location
 		@location = Location.find(params[:location])
 		@location.destroy
 	end
 
+	# route: GET /:user/:location
 	def forecast
 		@location = Location.find(params[:location])
 
