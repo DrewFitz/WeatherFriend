004984feeafe6841541f835ba117756d357d04e0
Drew Fitzpatrick <drwftz@gmail.com>
2016-09-29 11:58:15 -0400

    Updated styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
 
   root 'users#hello'
 
-  get  '/hello', to: 'users#hello'
-  post '/login', to: 'users#login'
-  get  '/new/:name', to: 'users#new'
-  post '/create/:name', to: 'users#create'
-  get  '/:name', to: 'users#show'
+  get  '/hello',              to: 'users#hello'
+  post '/login',              to: 'users#login'
+  get  '/new/:name',          to: 'users#new'
+  post '/create/:name',       to: 'users#create'
+  get  '/:name',              to: 'users#show'
   post '/:name/add_location', to: 'users#add_location'
-  get  '/:name/:location', to: 'users#forecast'
-  delete '/:name/:location', to: 'users#remove_location'
+  get  '/:name/:location',    to: 'users#forecast'
+  delete '/:name/:location',  to: 'users#remove_location'
 
 end
